d9b859ccf6794aed4dec4d4cce642f4aa8a83ea2
Sara Magdits <saramagdits@gmail.com>
2018-07-26 18:53:13 -0400

    Redesigned new and edit forms to match styling. Rewrote the form.js to assign id's to new inputs, allowing them to be targeted for deletion. Requires the user include at least one ingredient before submission. form.js now uses $(document).ready.

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,44 +1,26 @@
-//TODO fix the problem where the remove ingredient button only works on the first instance
-const init = function () {
-    let removeIngredientButton = document.getElementsByClassName("fa-minus-square");
-    let addIngredientButton = document.getElementById("addIngredient");
 
-    addIngredientButton.addEventListener("click", addIngredient);
+$(document).ready(function () {
+    // ADD INGREDIENTS
+    // i is used to assign an id to each new input group and delete button, so that it may be targeted for deletion later
+    let i = 1;
+    $("#add").click(function () {
+        i++;
+        $("#ingredientsList").append("<div id='ingredient" + i + "' class='input-group mb-3'><input type='text' class='form-control' placeholder='Add an ingredient' name='ingredients[]'><div class='input-group-append'><button id='" + i + "' class='btn-remove btn btn-outline-danger' type='button'><i class='fas fa-trash-alt'></i></button></div></div>");
+    });
 
-    for (let i = 0; i < removeIngredientButton.length; i++) {
-        let btn = removeIngredientButton[i];
-        btn.addEventListener("click", removeIngredient);
-    }
-};
+    // REMOVE INGREDIENTS
+    // delete the input group with the matching id
+    $(document).on("click", ".btn-remove", function () {
+        let buttonId = $(this).attr("id");
+        $("#ingredient" + buttonId + "").remove();
+    });
 
-/* Start - Event-Listeners */
-const removeIngredient = function (event) {
-    event.target.previousElementSibling.remove();
-    event.target.remove();
-};
-
-const addIngredient = function () {
-
-    let ingredientsList = document.getElementById("ingredientsList");
-    let newIngredient = document.createElement("input");
-    newIngredient.setAttribute("type", "text");
-    newIngredient.setAttribute("name", "ingredients[]");
-    newIngredient.setAttribute("class", "form-control");
-    newIngredient.setAttribute("placeholder", "Enter an ingredient");
-    ingredientsList.appendChild(newIngredient);
-
-    let removeIngredient = document.createElement("i");
-    removeIngredient.setAttribute("class", "far fa-minus-square fa-lg");
-    removeIngredient.addEventListener("click", removeIngredient);
-    ingredientsList.appendChild(removeIngredient);
-};
-//UPDATES FILE INPUT PLACEHOLDER 
-$("#img").on('change', function () {
-    //get the file name
-    let fileName = $(this).val().split('\\').pop();
-    //replace the "Choose a file" label
-    $(this).next('.custom-file-label').html(fileName);
+    //UPDATE FILE INPUT PLACEHOLDER
+    $("#img").on('change', function () {
+        //get the file name
+        let fileName = $(this).val().split('\\').pop();
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').html(fileName);
+    });
 });
-/* End - Event Listeners*/
 
-init();
